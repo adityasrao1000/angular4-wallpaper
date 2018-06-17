19b75e2fb4ba56604367bddd46c4e6d9de22bac9
Aditya Rao <adityasrao1000@gmail.com>
2018-06-17 08:41:30 +0530

    theme change and fixes

--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'nav-bar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['../app.component.scss']
+  styleUrls: ['../app.component.scss', 'navbar.component.scss']
 })
 
 export class NavbarComponent {
