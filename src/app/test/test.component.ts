30d90d50268980b986ed4b92422c6e56f4765fe6
Aditya Rao <adityasrao1000@gmail.com>
2018-06-17 07:41:15 +0530

    infinit scroll added

--- src/app/test/test.component.ts
+++ src/app/test/test.component.ts
@@ -11,21 +11,43 @@ import { Component } from '@angular/core';
 export class TestComponent {
 
   title = 'Categories';
-
+  categories = [];
+  index = 0;
   allcategories = [
-     {title: 'Charlotte', url: '/categories/charlotte', img: './assets/img/charlotte.jpg'},
-     {title: 'Code Geass', url: '/categories/codegeass', img: './assets/img/codegeass.jpg'},
-     {title: 'deathnote', url: '/categories/deathnote', img: './assets/img/deathnote.jpg'},
-     {title: 'durarara', url: '/categories/durarara', img: './assets/img/durarara.jpg'},
-     {title: 'naruto', url: '/categories/naruto', img: './assets/img/naruto.jpg'},
-     {title: 'onepiece', url: '/categories/onepiece', img: './assets/img/onepiece.png'},
-     {title: 'monster', url: '/categories/monster', img: './assets/img/monster.jpg'},
-     {title: 'noragami', url: '/categories/noragami', img: './assets/img/noragami.jpg'},
-     {title: 'vampire knight', url: '/categories/vampireknight', img: './assets/img/vampireknight.png'},
-     {title: 'ghost in shell', url: '/categories/ghostinshell', img: './assets/img/ghostinshell.jpg'},
-     {title: 'gundam seed', url: '/categories/gundamseed', img: './assets/img/gundamseed.jpg'},
-     {title: '.hack', url: '/categories/hack', img: './assets/img/hack.jpg'},
-     {title: 'inuyasha', url: '/categories/inuyasha', img: './assets/img/inuyasha.jpg'},
-     {title: 'trigun', url: '/categories/trigun', img: './assets/img/trigun.jpg'},
+    { title: 'Charlotte', url: '/categories/charlotte', img: './assets/img/charlotte.jpg' },
+    { title: 'Code Geass', url: '/categories/codegeass', img: './assets/img/codegeass.jpg' },
+    { title: 'deathnote', url: '/categories/deathnote', img: './assets/img/deathnote.jpg' },
+    { title: 'durarara', url: '/categories/durarara', img: './assets/img/durarara.jpg' },
+    { title: 'naruto', url: '/categories/naruto', img: './assets/img/naruto.jpg' },
+    { title: 'onepiece', url: '/categories/onepiece', img: './assets/img/onepiece.png' },
+    { title: 'monster', url: '/categories/monster', img: './assets/img/monster.jpg' },
+    { title: 'noragami', url: '/categories/noragami', img: './assets/img/noragami.jpg' },
+    { title: 'vampire knight', url: '/categories/vampireknight', img: './assets/img/vampireknight.png' },
+    { title: 'ghost in shell', url: '/categories/ghostinshell', img: './assets/img/ghostinshell.jpg' },
+    { title: 'gundam seed', url: '/categories/gundamseed', img: './assets/img/gundamseed.jpg' },
+    { title: '.hack', url: '/categories/hack', img: './assets/img/hack.jpg' },
+    { title: 'inuyasha', url: '/categories/inuyasha', img: './assets/img/inuyasha.jpg' },
+    { title: 'trigun', url: '/categories/trigun', img: './assets/img/trigun.jpg' },
+    { title: 'noragami', url: '/categories/noragami', img: './assets/img/noragami.jpg' },
+    { title: 'vampire knight', url: '/categories/vampireknight', img: './assets/img/vampireknight.png' }
   ];
+
+  constructor() {
+    for (this.index = 0; this.index < 5; this.index++) {
+      this.categories.push(this.allcategories[this.index]);
+    }
+  }
+
+  onScroll() {
+    let i = 0;
+    if (this.index === this.allcategories.length) { this.index = 0; }
+    while (this.index < this.allcategories.length) {
+      this.categories.push(this.allcategories[this.index]);
+      this.index++;
+      i++;
+      if (i === 4) {
+        break;
+      }
+    }
+  }
 }
