c4dbb15f11c27edb8d51e5f3f8f8e161d13ae30d
Aditya Rao <adityasrao1000@gmail.com>
2018-06-19 09:22:02 +0530

    components of the same page grouped together

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,11 +4,7 @@ import { FormsModule } from '@angular/forms';
 import { RouterModule, Routes } from '@angular/router';
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-import { ImageSliderComponent } from './image-slider/image-slider.component';
-import { ImgScrollerComponent } from './image-scroller/image-scroller.component';
 import { ArticleComponent } from './article/article.component';
-import { FeaturesComponent } from './features/features.component';
-import { VideoComponent, SafePipe } from './video/video.component';
 import { MainpageComponent } from './mainpage/mainpage.component';
 import { routes } from './app-routing.module';
 import { WallpapersComponent } from './wallpaper/wallpapers.component';
@@ -18,10 +14,14 @@ import { NotFoundComponent } from './pagenotfound/pagenotfound.component';
 import { HttpService } from './http/http.service';
 import { HttpClientModule } from '@angular/common/http';
 import { InfiniteScrollModule } from 'ngx-infinite-scroll';
-import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import { NgbModule } from '@ng-bootstrap/ng-bootstrap';
 import { SearchPipe } from './navbar/search.pipe';
 import { FooterComponent } from './footer/footer.component';
 import { CategoriesComponent } from './categories/categories.component';
+import { ImageSliderComponent } from './mainpage/image-slider/image-slider.component';
+import { ImgScrollerComponent } from './mainpage/image-scroller/image-scroller.component';
+import { VideoComponent, SafePipe } from './mainpage/video/video.component';
+import { FeaturesComponent } from './mainpage/features/features.component';
 
 @NgModule({
   imports: [
