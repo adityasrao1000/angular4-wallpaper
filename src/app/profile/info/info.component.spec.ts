1825fa6d5d79e1915f718e2e959030f36d08c488
Aditya Rao <adityasrao1000@gmail.com>
2018-07-23 23:13:16 +0530

    changes made

--- src/app/profile/info/info.component.spec.ts
+++ src/app/profile/info/info.component.spec.ts
@@ -1,6 +1,20 @@
 import { TestBed, ComponentFixture, inject, async } from '@angular/core/testing';
 import { InfoComponent } from './info.component';
+import { UserCredentialsService } from '../../utils/user-credentials.service';
+import { HttpClientModule } from '@angular/common/http';
+import { AuthServiceConfig, GoogleLoginProvider, AuthService } from 'ng4-social-login';
+import { ValidateOauthService } from '../../utils/validate-oauth.service';
 
+const CONFIG = new AuthServiceConfig([
+    {
+        id: GoogleLoginProvider.PROVIDER_ID,
+        provider: new GoogleLoginProvider('335091079574-vtlsr39j241l0tpn5rvmmvp2dksanhbs.apps.googleusercontent.com')
+    }
+], false);
+
+export function provideConfig() {
+    return CONFIG;
+}
 describe('Component: InfoComponent', () => {
 
     let component: InfoComponent;
@@ -10,7 +24,16 @@ describe('Component: InfoComponent', () => {
 
         // refine the test module by declaring the test component
         TestBed.configureTestingModule({
-            declarations: [InfoComponent]
+            imports: [HttpClientModule],
+            declarations: [InfoComponent],
+            providers: [
+                UserCredentialsService,
+                {
+                    provide: AuthServiceConfig,
+                    useFactory: provideConfig
+                },
+                AuthService,
+                ValidateOauthService]
         });
 
         // create component and test fixture
