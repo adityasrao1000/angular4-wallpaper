e01744a34dfd422e5792b52c614791e9c24641de
Aditya Rao <adityasrao1000@gmail.com>
2018-08-20 19:44:41 +0530

    featured content added

--- src/app/communities/featured/featured.component.ts
+++ src/app/communities/featured/featured.component.ts
@@ -6,5 +6,12 @@ import { Component } from '@angular/core';
 })
 
 export class FeaturedComponent {
-
+    featured = [
+        { image: './assets/img/charlotte.jpg', name: 'Tomiomi hawk ^-^', description: 'lorsem dorsi le mariano fhmkkfmr fjrf', members: 3 },
+        { image: './assets/img/codegeass.jpg', name: 'Honi Honi uta', description: 'Suhagin e lambda forci', members: 15 },
+        { image: './assets/img/deathnote.jpg', name: 'death note ja buhula ;)', description: 'sdjnnjdnjrdnjccn rifrjf', members: 12 },
+        { image: './assets/img/bleach.jpg', name: 'memerse of bleach lovers', description: 'jhckdjcn hdhrh rkjfkrfmkfmkcn n', members: 99 },
+        { image: './assets/img/naruto.jpg', name: 'Bobi no maru', description: 'members de jinaio', members: 404 },
+        { image: './assets/img/trigun.jpg', name: 'FAN PAGE', description: '', members: 122 }
+    ];
 }
