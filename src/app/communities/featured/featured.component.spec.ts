59ef9278a9c58c3de236d16fec12250d81f3d841
Aditya Rao <adityasrao1000@gmail.com>
2018-08-22 12:12:40 +0530

    changes made

--- src/app/communities/featured/featured.component.spec.ts
+++ src/app/communities/featured/featured.component.spec.ts
@@ -26,4 +26,61 @@ describe('Component: FeaturedComponent', () => {
     it('should create component', () => {
         expect(component).toBeTruthy();
     });
+
+    it('filter should be toggled', () => {
+        component.toggleFilter();
+        expect(component.filterShow).toBe(true);
+    });
+
+    it('should be sorted by members', () => {
+        component.featured = [
+            // tslint:disable-next-line:max-line-length
+            { image: './assets/img/charlotte.jpg', name: 'Tomiomi hawk ^-^', description: 'lorsem dorsi le mariano fhmkkfmr fjrf', members: 3 },
+            { image: './assets/img/codegeass.jpg', name: 'Honi Honi uta', description: 'Suhagin e lambda forci', members: 15 },
+            // tslint:disable-next-line:max-line-length
+            { image: './assets/img/charlotte.jpg', name: 'Tomiomi hawk ^-^', description: 'lorsem dorsi le mariano fhmkkfmr fjrf', members: 3 },
+            // tslint:disable-next-line:max-line-length
+            { image: './assets/img/bleach.jpg', name: 'memerse of bleach lovers', description: 'jhckdjcn hdhrh rkjfkrfmkfmkcn n', members: 99 },
+            { image: './assets/img/naruto.jpg', name: 'Bobi no maru', description: 'members de jinaio', members: 404 },
+            { image: './assets/img/trigun.jpg', name: 'FAN PAGE', members: 122 },
+        ];
+        component.sortByMembers();
+        fixture.detectChanges();
+        const sorted = [
+            { image: './assets/img/naruto.jpg', name: 'Bobi no maru', description: 'members de jinaio', members: 404 },
+            { image: './assets/img/trigun.jpg', name: 'FAN PAGE', members: 122 },
+            // tslint:disable-next-line:max-line-length
+            { image: './assets/img/bleach.jpg', name: 'memerse of bleach lovers', description: 'jhckdjcn hdhrh rkjfkrfmkfmkcn n', members: 99 },
+            { image: './assets/img/codegeass.jpg', name: 'Honi Honi uta', description: 'Suhagin e lambda forci', members: 15 },
+            // tslint:disable-next-line:max-line-length
+            { image: './assets/img/charlotte.jpg', name: 'Tomiomi hawk ^-^', description: 'lorsem dorsi le mariano fhmkkfmr fjrf', members: 3 },
+            // tslint:disable-next-line:max-line-length
+            { image: './assets/img/charlotte.jpg', name: 'Tomiomi hawk ^-^', description: 'lorsem dorsi le mariano fhmkkfmr fjrf', members: 3 }
+        ];
+        expect(component.featured).toEqual(sorted);
+    });
+
+    it('should be sorted by name', () => {
+        component.featured = [
+            // tslint:disable-next-line:max-line-length
+            { image: './assets/img/charlotte.jpg', name: 'bomiomi hawk ^-^', description: 'lorsem dorsi le mariano fhmkkfmr fjrf', members: 3 },
+            { image: './assets/img/codegeass.jpg', name: 'aoni Honi uta', description: 'Suhagin e lambda forci', members: 15 },
+            // tslint:disable-next-line:max-line-length
+            { image: './assets/img/charlotte.jpg', name: 'comiomi hawk ^-^', description: 'lorsem dorsi le mariano fhmkkfmr fjrf', members: 3 },
+            // tslint:disable-next-line:max-line-length
+            { image: './assets/img/charlotte.jpg', name: 'comiomi hawk ^-^', description: 'lorsem dorsi le mariano fhmkkfmr fjrf', members: 3 }
+        ];
+        component.sortByName();
+        fixture.detectChanges();
+        const sorted = [
+            { image: './assets/img/codegeass.jpg', name: 'aoni Honi uta', description: 'Suhagin e lambda forci', members: 15 },
+             // tslint:disable-next-line:max-line-length
+             { image: './assets/img/charlotte.jpg', name: 'bomiomi hawk ^-^', description: 'lorsem dorsi le mariano fhmkkfmr fjrf', members: 3 },
+             // tslint:disable-next-line:max-line-length
+             { image: './assets/img/charlotte.jpg', name: 'comiomi hawk ^-^', description: 'lorsem dorsi le mariano fhmkkfmr fjrf', members: 3 },
+             // tslint:disable-next-line:max-line-length
+             { image: './assets/img/charlotte.jpg', name: 'comiomi hawk ^-^', description: 'lorsem dorsi le mariano fhmkkfmr fjrf', members: 3 }
+        ];
+        expect(component.featured).toEqual(sorted);
+    });
 });
